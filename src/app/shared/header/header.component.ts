d464aa3064ce62662b80fad99d6c1023ed1300be
Nitesh Singh <niteshsingh7463@gmail.com>
2021-06-28 15:39:36 +0530

    theming using bootstrap

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -14,67 +14,9 @@ export class HeaderComponent implements OnInit {
     console.log('theme change worked');
     this.isDark = !this.isDark;
     if (this.isDark) {
-      document.documentElement.style.setProperty('--clr-green-light', 'black');
-      document.documentElement.style.setProperty('--moon', 'var(--clr-white)');
-      document.documentElement.style.setProperty(
-        '--section1background',
-        'var(--clr-grey-1)'
-      );
-      document.documentElement.style.setProperty(
-        '--section2background',
-        'var(--clr-black)'
-      );
-
-      document.documentElement.style.setProperty(
-        '--btnbackground',
-        'var(--clr-white)'
-      );
-      document.documentElement.style.setProperty(
-        '--btntextcolor',
-        'var(--clr-black)'
-      );
-      document.documentElement.style.setProperty(
-        '--textcolor',
-        'var(--clr-white)'
-      );
-      document.documentElement.style.setProperty(
-        '--section3icons',
-        'var(--clr-white)'
-      );
+      document.querySelector('body').setAttribute('theme', 'bootstrap-dark');
+    } else {
+      document.querySelector('body').removeAttribute('theme');
     }
-    // =======================================
-    else {
-      document.documentElement.style.setProperty(
-        '--clr-green-light',
-        ' hsl(125, 71%, 66%)'
-      );
-      document.documentElement.style.setProperty('--moon', 'var(--clr-black)');
-      document.documentElement.style.setProperty(
-        '--section1background',
-        'var(--clr-grey-10)'
-      );
-      document.documentElement.style.setProperty(
-        '--section2background',
-        'var(--clr-white)'
-      );
-
-      document.documentElement.style.setProperty(
-        '--btnbackground',
-        'var(--clr-green-light)'
-      );
-      document.documentElement.style.setProperty(
-        '--btntextcolor',
-        'var(--clr-white)'
-      );
-      document.documentElement.style.setProperty(
-        '--textcolor',
-        'var(--clr-black)'
-      );
-      document.documentElement.style.setProperty(
-        '--section3icons',
-        'var(--clr-green-light)'
-      );
-    }
-    // =============================================
   }
 }
